b3c02e59eadd23c455a45309c5ddfb44698cd348
yeucp <43937588+yeucp@users.noreply.github.com>
2024-01-10 11:29:50 -0600

    [Cleaning]

--- src/Infrastructure/Authentication/JwtSettings.cs
+++ src/Infrastructure/Authentication/JwtSettings.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Infrastructure.Authentication
+﻿namespace Infrastructure.Authentication
 {
     public class JwtSettings
     {
